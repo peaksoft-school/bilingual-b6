a69619ea4fbcd242030010221e7c1dff0e4fe80e
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-10-03 16:14:30 +0600

    Create empty project

---
+++ src/test/java/kg/peaksoft/bilingualb6/BilingualB6ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.peaksoft.bilingualb6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilingualB6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
