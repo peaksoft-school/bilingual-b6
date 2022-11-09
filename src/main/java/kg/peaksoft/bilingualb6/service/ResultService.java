78fdb3d2221d99efd7a3b9e99c12e701f03ae012
Nurisa <nurisa.abdykalykova03@gmail.com>
2022-11-09 18:55:58 +0600

    refactor result

--- src/main/java/kg/peaksoft/bilingualb6/service/ResultService.java
+++ src/main/java/kg/peaksoft/bilingualb6/service/ResultService.java
@@ -1,13 +1,8 @@
 package kg.peaksoft.bilingualb6.service;
 
 import kg.peaksoft.bilingualb6.dto.response.ResultResponses;
-import kg.peaksoft.bilingualb6.entites.Client;
-import kg.peaksoft.bilingualb6.exceptions.NotFoundException;
-import kg.peaksoft.bilingualb6.repository.ClientRepository;
 import kg.peaksoft.bilingualb6.repository.ResultRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
