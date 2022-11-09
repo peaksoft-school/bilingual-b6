0dd01ad25ae627db43b9fdaf9e3b44d906c7b89e
ACER <k>
2022-11-09 16:52:41 +0600

    bug fixed

--- src/main/java/kg/peaksoft/bilingualb6/api/ResultController.java
+++ src/main/java/kg/peaksoft/bilingualb6/api/ResultController.java
@@ -3,12 +3,10 @@ package kg.peaksoft.bilingualb6.api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kg.peaksoft.bilingualb6.dto.response.ResultResponses;
-import kg.peaksoft.bilingualb6.entites.Result;
 import kg.peaksoft.bilingualb6.service.ResultService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
