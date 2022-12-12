f2f396c221bba5cfd974f100d785510d1d3059e5
arlen34 <arlen.kubanychbekovv01@gmail.com>
2022-12-13 00:23:14 +0300

    view result

--- src/main/java/kg/peaksoft/bilingualb6/api/ResultController.java
+++ src/main/java/kg/peaksoft/bilingualb6/api/ResultController.java
@@ -2,7 +2,8 @@ package kg.peaksoft.bilingualb6.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import kg.peaksoft.bilingualb6.dto.response.ResultResponses;
+import kg.peaksoft.bilingualb6.dto.request.EvaluationRequest;
+import kg.peaksoft.bilingualb6.dto.response.*;
 import kg.peaksoft.bilingualb6.entites.AuthInfo;
 import kg.peaksoft.bilingualb6.service.ResultService;
 import lombok.RequiredArgsConstructor;
@@ -46,5 +47,45 @@ public class ResultController {
     @DeleteMapping("/{id}")
     public List<ResultResponses> delete(@PathVariable Long id, Principal principal){
        return resultService.deleteResult(id,principal);
+    }
+
+    @Operation(summary = "Get all results",
+            description = "Get all clients results(for admin)")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/view-results")
+    public List<ViewAllResultResponse> getAllResult(){
+        return resultService.viewResults();
+    }
+
+    @Operation(summary = "Get client result",
+            description = "Get the result of a specific client's question(for admin)")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/view-result-questions/{id}")
+    public ViewResultResponse resultResponse(@PathVariable Long id){
+        return resultService.getResultResponse(id);
+    }
+
+    @Operation(summary = "Get client answer",
+            description = "Get client answer for evaluation(for admin)")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("/view-client-answer/{id}")
+    public CheckQuestionAnswerResponse answerResponse(@PathVariable Long id){
+        return resultService.getAnswerForEvaluation(id);
+    }
+
+    @Operation(summary = "Evaluate client answer",
+            description = "Evaluate client question answer and give a score (for admin)")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @PostMapping("/evaluate-client-answer")
+    public ViewResultResponse viewResultResponse(@RequestBody EvaluationRequest request){
+        return resultService.giveScoreForQuestion(request);
+    }
+
+    @Operation(summary = "Delete result",
+            description = "Client method that deletes its results(for client)")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @DeleteMapping("/client-results/{id}")
+    public List<ViewAllResultResponse> delete(@PathVariable Long id){
+        return resultService.delete(id);
     }
 }
